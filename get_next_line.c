11acecb4f5cdc13b8b6100e5a0d7fa688b07a25b
kychoi <kychoi@e1r1p8.42.fr>
2021-12-02 10:11:24 +0100

    fix:version pourri

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
+/*   By: kychoi <kychoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/30 21:21:49 by kyubongchoi       #+#    #+#             */
-/*   Updated: 2021/12/02 08:31:53 by kyubongchoi      ###   ########.fr       */
+/*   Created: 2021/12/02 09:58:55 by kychoi            #+#    #+#             */
+/*   Updated: 2021/12/02 10:09:54 by kychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 #include<stdio.h>//TO_REMOVE
 #include<string.h>//TO_REMOVE
-
 char	*get_next_line(int fd)
 {
 	char	*buffer;
@@ -35,16 +34,17 @@ char	*get_next_line(int fd)
 	if (i == 0)
 		return (NULL);
 	buffer[i] = 0;
-	printf("[%zu]%s(strlen:%lu)\n", i, buffer, strlen(buffer));
+	// printf("[%zu]%s(strlen:%lu)\n", i, buffer, strlen(buffer));
 	return (buffer);
 }
 
+/*
 int	main(void)
 {
 	int	fd;
 
 	fd = open("./test.txt", O_RDONLY);
-	/*output*/
+	//output
 	printf("(1st exec)		:%s\n", get_next_line(fd));
 	printf("(2st exec)		:%s\n", get_next_line(fd));
 	printf("(3st exec)		:%s\n", get_next_line(fd));
@@ -53,7 +53,7 @@ int	main(void)
 	printf("(6st exec)		:%s\n", get_next_line(fd));
 	printf("(7st exec)		:%s\n", get_next_line(fd));
 
-	/*compare between output and expected*/
+	//compare between output and expected
 	// printf("(1st exec)		:%d\n", strcmp(get_next_line(fd), "1st line"));
 	// printf("(2nd exec)		:%d\n", strcmp(get_next_line(fd), "2nd line"));
 	// printf("(3rd exec)		:%d\n", strcmp(get_next_line(fd), "3rd line"));
@@ -63,7 +63,7 @@ int	main(void)
 	while (1);
 	return (0);
 }
-
+*/
 //bonus??
 /*
 void	*ft_realloc(void *ptr, size_t size)
@@ -90,11 +90,11 @@ char	*get_next_line(int fd)
 		if (tmp == '\n')
 		{
 			buffer[i] = '\0';
-			printf("[%zu]%d(strlen:%lu)\n", i, buffer[i], strlen(buffer));
+			// printf("[%zu]%d(strlen:%lu)\n", i, buffer[i], strlen(buffer));
 			return (buffer);
 		}
 		buffer[i++] = tmp;
-		printf("[%zu]%c\n", i - 1, buffer[i - 1]);
+		// printf("[%zu]%c\n", i - 1, buffer[i - 1]);
 	}
 	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,4 +3,7 @@
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
+
+char	*get_next_line(int fd);
+
 #endif
