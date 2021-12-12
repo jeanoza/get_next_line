ccd4f6fc8d7cb7a5ae3774d906e72c9bf60016ba
jeanoza <choikb916@gmail.com>
2021-12-12 17:33:44 +0100

    fix:timeout managed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 09:58:55 by kychoi            #+#    #+#             */
-/*   Updated: 2021/12/12 13:13:15 by kyubongchoi      ###   ########.fr       */
+/*   Updated: 2021/12/12 17:26:14 by kyubongchoi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,55 +21,50 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len;
 	size_t	i;
 
-	if (s1)
-		len = ft_strlen(s1);
-	if (s2)
-		len = ft_strlen(s1) + ft_strlen(s2);
+	len = ft_strlen(s1) + ft_strlen(s2);
 	result = malloc(sizeof(char) * (len + 1));
 	if (!result)
+	{
+		free((char *)s1);
 		return (NULL);
+	}
 	len = 0;
 	while (s1 && s1[len])
 	{
 		result[len] = s1[len];
 		++len;
 	}
-	i = -1;
-	while (s2 && s2[++i])
-		result[len++] = s2[i];
+	free((char *)s1);
+	i = 0;
+	while (s2 && s2[i])
+		result[len++] = s2[i++];
 	result[len] = '\0';
 	return (result);
 }
 
-// char	*ft_select_nl(char *buffer, int *cursor)
-char	*ft_select_nl(char *buffer)
+char	*ft_select_nl(char *buffer, char *new_line)
 {
 	int		i;
-	int		is_nl;
+	int		len;
 	char	*dst;
 
 	if (buffer[0] == '\0')
 		return (NULL);
 	if (buffer[0] == '\n')
 		return (ft_strndup("\n", 1));
-
-	is_nl = 0;
-	if (ft_strchr(buffer, '\n'))
-		is_nl = 1;
-	i = 0;
-	while (buffer && buffer[i] && buffer[i] != '\n')
-		++i;
-	dst = malloc(sizeof(char) * (i + 1 + is_nl));
+	if (new_line)
+		len = new_line - buffer + 1;
+	else
+		len = ft_strlen(buffer);
+	dst = malloc(sizeof(char) * (len + 1));
 	if (!dst)
 		return (NULL);
 	i = 0;
-	while (buffer && buffer[i] && buffer[i] != '\n')
+	while (i < len && buffer[i])
 	{
 		dst[i] = buffer[i];
 		++i;
 	}
-	if (is_nl)
-		dst[i++] = '\n';
 	dst[i] = 0;
 	return (dst);
 }
@@ -80,6 +75,11 @@ char	*ft_move_cursor(char *buffer)
 	int		j;
 	char	*dst;
 
+	if (buffer && (buffer[0] == 0 || ft_strlen(buffer) == 1))
+	{
+		free(buffer);
+		return (NULL);
+	}
 	j = 0;
 	while (buffer && (buffer)[j] && (buffer)[j] != '\n')
 		++j;
@@ -93,39 +93,35 @@ char	*ft_move_cursor(char *buffer)
 		++i;
 	}
 	dst[i] = 0;
+	free(buffer);
 	return (dst);
 }
+
 char	*get_next_line(int fd)
 {
 	char		buffer[BUFFER_SIZE + 1];
 	int			cursor;
 	static char	*backup;
-	char		*tmp;
-	char		*line;
+	char		*current;
+	char		*new_line;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	cursor = BUFFER_SIZE;
-	while (cursor > 0 && !ft_strchr(backup, '\n'))
+	while (cursor > 0)
 	{
 		cursor = read(fd, buffer, BUFFER_SIZE);
 		if (cursor == -1)
 			return (NULL);
 		buffer[cursor] = 0;
-		tmp = backup;
-		backup = ft_strjoin(tmp, buffer);
-		free(tmp);
+		backup = ft_strjoin(backup, buffer);
+		if (cursor == 0 && ft_strchr(backup, '\n'))
+			break ;
 	}
-	line = ft_select_nl(backup);
-	tmp = backup;
+	new_line = ft_strchr(backup, '\n');
+	current = ft_select_nl(backup, new_line);
 	backup = ft_move_cursor(backup);
-	if (cursor == 0 && backup && backup[0] == 0)
-	{
-		free(backup);
-		backup = NULL;
-	}
-	free(tmp);
-	return (line);
+	return (current);
 }
 /*
 int	main(void)
@@ -152,6 +148,8 @@ int	main(void)
 
 	char *str7 = get_next_line(fd);
 	printf("(7th exec)		:%s\n\n", str7);
+	// while (1)
+	// 	;
 	close(fd);
 	return (0);
 }
